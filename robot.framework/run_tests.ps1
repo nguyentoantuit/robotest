7d51881ae9a611f9735ad03588d7142bd745dc56
Toan Nguyen <toan-huu.nguyen@kolekto.nl>
2019-04-21 18:15:08 +0700

    Use docker to run test suite

---
+++ robot.framework/run_tests.ps1
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker run --rm -e USERNAME="Ipatios Asmanidis" --net=host -v ${PWD}\output:/output -v ${PWD}\suites:/suites -v ${PWD}\scripts:/scripts -v ${PWD}\reports:/reports --security-opt seccomp:unconfined --shm-size "256M" "ypasmk/robot-framework"
