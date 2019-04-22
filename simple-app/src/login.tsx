115e1c958aee43d89c3c60e1d52b4583a3589a88
Toan Nguyen <toan-huu.nguyen@kolekto.nl>
2019-04-22 09:56:52 +0700

    Update sample test site

--- simple-app/src/login.tsx
+++ simple-app/src/login.tsx
@@ -30,16 +30,20 @@ class LoginComponent extends Component<any, any> {
             <div>
                 <table>
                     <tr>
+                        <td></td>
+                        <td><label >Username/password: demo/1234</label></td>
+                    </tr>
+                    <tr>
                         <td><label >User Name:</label></td>
-                        <td><input id="username_field" size={30} type="text" ref={this._username}/></td>
+                        <td><input id="username_field" size={30} type="text" ref={this._username} /></td>
                     </tr>
                     <tr>
                         <td><label>Password:</label></td>
-                        <td><input id="password_field" size={30} type="password" ref={this._password}/></td>
+                        <td><input id="password_field" size={30} type="password" ref={this._password} /></td>
                     </tr>
                     <tr>
                         <td>&nbsp;</td>
-                        <td><input id="login_button" type="submit" value="LOGIN" onClick={this.login}/></td>
+                        <td><input id="login_button" type="submit" value="LOGIN" onClick={this.login} /></td>
                     </tr>
                 </table>
             </div>
